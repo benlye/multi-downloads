43dac1bd30f088873b2e17c8e87924f5bfd9a594
Ben Lye <ben@lye.co.nz>
2020-01-26 19:17:27 +0000

    Release notes and permalink

--- docs/script.js
+++ docs/script.js
@@ -1,3 +1,13 @@
+function bodyLoad() {
+    myFunction();
+    var url = window.location.toString();
+    var parts = url.split("?")
+    if (parts.length > 1) {
+        // alert(parts[1]);
+        $('#multiModuleSelect').val(parts[1]).trigger('change');
+    }
+}
+
 function myFunction() {
     var input, filter, table, tr, td, i, txtValue;
     moduleType = document.getElementById("moduleType").value.toUpperCase();
@@ -31,33 +41,43 @@ function myFunction() {
             }
         }
     }
+
+    moduleModel = document.getElementById("multiModuleSelect").value.toLowerCase();
+    if (moduleModel != '') {
+        // document.getElementById("moduleLinkUrl").href = "https://downloads.multi-module.org/?" + moduleModel;
+        document.getElementById("moduleLinkUrlDisplay").innerHTML = "https://downloads.multi-module.org/?" + moduleModel;
+        document.getElementById("modulelink").hidden = false;
+    } else {
+        document.getElementById("moduleLinkUrlDisplay").innerHTML = null;
+        document.getElementById("modulelink").hidden = true;
+    }
 }
 
 function moduleSelect() {
     selectedModule = document.getElementById("multiModuleSelect").value.toLowerCase();
     switch(selectedModule) {
-        case "banggood-avr":
+        case "bg-avr":
         case "diy-avr":
             document.getElementById("moduleType").value = "-avr-";
             document.getElementById("radioType").value = "";
             document.getElementById("telemetryInversion").value = "-inv-";
             break;
-        case "banggood-stm32":
+        case "bg-stm32":
         case "diy-stm32":
-        case "hobbyporter-stm32":
-        case "irangex-irx":
-        case "jumper-jp4in1":
+        case "hp4in1":
+        case "irangex":
+        case "jp4in1":
         case "vantac-mpm":
             document.getElementById("moduleType").value = "-stm-";
             document.getElementById("radioType").value = "";
             document.getElementById("telemetryInversion").value = "-inv-";
             break;
-        case "jumper-t16ext":
+        case "jp-t16ext":
             document.getElementById("moduleType").value = "-stm-";
             document.getElementById("radioType").value = "-opentx-";
             document.getElementById("telemetryInversion").value = "-inv-";
             break;
-        case "jumper-t16int":
+        case "jp-t16int":
             document.getElementById("moduleType").value = "-stm-";
             document.getElementById("radioType").value = "-opentx-";
             document.getElementById("telemetryInversion").value = "-noinv-";
@@ -84,6 +104,34 @@ function formReset() {
     }, 100); 
 }
 
+function openReleaseNotes() {
+    firmwareVersion = document.getElementById("firmwareVersion").value.toLowerCase();
+    url = 'https://github.com/pascallanger/DIY-Multiprotocol-TX-Module/releases/tag/' + firmwareVersion;
+    window.open(url,'_new');
+}
+
+function copyToClipboard(elementId) {
+
+    // Create an auxiliary hidden input
+    var aux = document.createElement("input");
+  
+    // Get the text from the element passed into the input
+    aux.setAttribute("value", document.getElementById(elementId).innerHTML);
+  
+    // Append the aux input to the body
+    document.body.appendChild(aux);
+  
+    // Highlight the content
+    aux.select();
+  
+    // Execute the copy command
+    document.execCommand("copy");
+  
+    // Remove the input from the body
+    document.body.removeChild(aux);
+  
+  }
+  
 $(document).ready(function(){
     $("[data-toggle=popover]").popover({
         html: true, 
